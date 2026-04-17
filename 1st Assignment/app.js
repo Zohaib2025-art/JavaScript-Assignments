8198722696f7b99c06cfb8a4e29cc2761e0c7909
Zohaib-1234-max <abdulzobi412@gmail.com>
2026-04-17 11:54:22 +0500

    First assignment uploaded

---
+++ 1st Assignment/app.js
@@ -0,0 +1,12 @@
+// Question 1
+
+// alert("Welcome");
+
+// Question 2
+
+// alert("Error! Please enter a valid password");
+
+//Question 3
+
+alert("Welcome to JS land page");
+alert("Happy Coding");
